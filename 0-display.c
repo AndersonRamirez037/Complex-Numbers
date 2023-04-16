6caef2e0459f0aacd14870da8ab1e0c12677c89d
AndersonRamirez037 <andersongamer037@gmail.com>
2023-04-15 21:16:53 -0500

    0. Structure Complex

--- 0-display.c
+++ 0-display.c
@@ -1,12 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 void display_complex_number(complex c){
-    if(c.re != 0 && c.im != 0){
+    if(c.im != 0){
         printf("%.0f + %.0fi\n",c.re,c.im);
-        if(c.re == 0 && c.im != 0){
-            printf("%.0f\n",c.im);
-        } else{
-            printf("%.0f\n",c.re);
-        }
+    } else{
+        printf("%.0f\n",c.re);
     }
 }
